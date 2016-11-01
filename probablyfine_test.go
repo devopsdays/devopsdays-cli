118199e3a94f017309e01cf0b0b0e9690a3cad35
Matt Stratton <matt.stratton@gmail.com>
2016-11-01 10:00:21 -0500

    Add tests for validateField()

--- probablyfine_test.go
+++ probablyfine_test.go
@@ -17,3 +17,24 @@ func TestEventDataPath(t *testing.T) {
 		t.Error("Response from eventDataPath is an unexpected value")
 	}
 }
+
+func TestValidateField(t *testing.T) {
+	if v := validateField("Chicago", "city"); v != true {
+		t.Error("Valid city did not pass validation test in validateField")
+	}
+	if v := validateField("3yl0RmG1wU8q5TeDPKZEsNU3E54nyYf5MNhGhzqcxhoLJkeckXCa1saWCPM24YhwIteGEUjLW8S715WkoDvt3vFsMaVeYXCUZWNL", "city"); v == true {
+		t.Error("Invalid city passed validation test in validateField")
+	}
+	if v := validateField("2016", "year"); v != true {
+		t.Error("Valid year did not pass validation test in validateField")
+	}
+	if v := validateField("19008", "year"); v == true {
+		t.Error("Invalid year passed validation test in validateField")
+	}
+	if v := validateField("devopsdays", "twitter"); v != true {
+		t.Error("Valid twitter did not pass validation test in validateField")
+	}
+	if v := validateField("devops days", "twitter"); v == true {
+		t.Error("Invalid twitter passed validation test in validateField")
+	}
+}
