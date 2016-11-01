7227eb89c922a88612aac3f80dbb54fcca98bee8
Matt Stratton <matt.stratton@gmail.com>
2016-11-01 05:13:28 -0500

    Add tests for cityClean and eventDataPath

--- probablyfine_test.go
+++ probablyfine_test.go
@@ -2,12 +2,18 @@ package main
 
 import "testing"
 
-func TestAddEvent(t *testing.T) {
-	addEvent()
+func TestCityClean(t *testing.T) {
+	cleanCity := cityClean("New York")
 
+	if cleanCity != "new-york" {
+		t.Error("Response from cityClean is unexpected value")
+	}
 }
 
-func TestAddSponsor(t *testing.T) {
-	addSponsor()
+func TestEventDataPath(t *testing.T) {
+	testDataPath := eventDataPath("New York", "2018")
 
+	if testDataPath != "/Users/mattstratton/src/devopsdays-web/data/events/2018-new-york.yml" {
+		t.Error("Response from eventDataPath is an unexpected value")
+	}
 }
