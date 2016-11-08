58286181f17b003e92a7309c42ac2c66fc812802
Matt Stratton <matt.stratton@gmail.com>
2016-11-08 13:17:23 -0600

    Update version command to remove Hugo

--- cmd/version.go
+++ cmd/version.go
@@ -17,8 +17,8 @@ func init() {
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Print the version number of Hugo",
-	Long:  `All software has versions. This is Hugo's`,
+	Short: "Print the version number of Probably Fine",
+	Long:  `All software has versions. This is ours.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Version: ", Version)
 		fmt.Println("Build Time: ", Build)
