4e7f0a1e72f4e950803ee1060713e2a1268396af
Matt Stratton <matt.stratton@gmail.com>
2016-11-08 10:15:32 -0600

    Add version command

--- cmd/sponsor_test.go
+++ cmd/sponsor_test.go
@@ -5,6 +5,7 @@ import "testing"
 func TestSponsorDataPath(t *testing.T) {
 	t.Log("Testing evaluation of sponsor data file path")
 	if v := sponsorDataPath("/Users/mattstratton/src/devopsdays-web", "chef"); v != "/Users/mattstratton/src/devopsdays-web/data/sponsors/chef.yml" {
-		t.Error("Response from eventDataPath is an unexpected value")
+		t.Log("Response from eventDataPath is an unexpected value")
+		t.Fail()
 	}
 }
