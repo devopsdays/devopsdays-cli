e54d8b4096dd666c763353eb5f716b3849c5b2d5
Matt Stratton <matt.stratton@gmail.com>
2017-04-18 18:25:14 -0400

    remove goveralls because travis failing

--- cmd/editEvent.go
+++ cmd/editEvent.go
@@ -145,26 +145,6 @@ type Organizer struct {
 	Bio      string
 }
 
-func fieldMap() (fieldMap map[string]string) {
-	tempMap := make(map[string]string)
-	tempMap["EventTwitter"] = "Twitter"
-	tempMap["GaTrackingID"] = "Google Analytics Tracking ID"
-	tempMap["Startdate"] = "Start Date"
-	tempMap["Enddate"] = "End Date"
-	tempMap["CfpDateStart"] = "CFP Start Date"
-	tempMap["CfpDateEnd"] = "CFP End Date"
-	tempMap["CfpDateAnnounce"] = "CFP Announcement Date"
-	tempMap["CfpOpen"] = "CFP Link"
-	tempMap["RegistrationDateStart"] = "Registation Start Date"
-	tempMap["RegistrationDateEnd"] = "Registration End Date"
-	tempMap["RegistrationLink"] = "Registration Link"
-	tempMap["Coordinates"] = "Coordinates"
-	tempMap["Location"] = "Location"
-	tempMap["LocationAddress"] = "Location Address"
-
-	return tempMap
-}
-
 func organizerFieldMap() (fieldMap map[string]string) {
 	tempMap := make(map[string]string)
 	tempMap["Name"] = "Organizer Name"
