33b2170db5f70c92a505e0d44581dc4b04fe3fcd
Matt Stratton <matt.stratton@gmail.com>
2017-10-04 15:23:58 -0500

    Fix spacing error in speaker template

--- create/templates.go
+++ create/templates.go
@@ -92,15 +92,15 @@ sponsor_levels:
     `
 
 const speakerTmpl = `+++
-    Title = "{{ .Title }}"
-    type = "speaker"
-    {{ with .Website }}website = "{{ . }}"{{ end }}
-    {{ with .Twitter }}twitter = "{{ . }}"{{ end }}
-    {{ with .Facebook }}facebook = "{{ . }}"{{ end }}
-    {{ with .Linkedin }}linkedin = "{{ . }}"{{ end }}
-    {{ with .Github }}github = "{{ . }}"{{ end }}
-    {{ with .Gitlab }}gitlab = "{{ . }}"{{ end }}
-    {{ with .ImagePath }}image = "{{ . }}"{{ end }}
-    +++
-    {{ with .Bio }}{{.}}{{ end }}
-    `
+Title = "{{ .Title }}"
+type = "speaker"
+{{ with .Website }}website = "{{ . }}"{{ end }}
+{{ with .Twitter }}twitter = "{{ . }}"{{ end }}
+{{ with .Facebook }}facebook = "{{ . }}"{{ end }}
+{{ with .Linkedin }}linkedin = "{{ . }}"{{ end }}
+{{ with .Github }}github = "{{ . }}"{{ end }}
+{{ with .Gitlab }}gitlab = "{{ . }}"{{ end }}
+{{ with .ImagePath }}image = "{{ . }}"{{ end }}
++++
+{{ with .Bio }}{{.}}{{ end }}
+`
