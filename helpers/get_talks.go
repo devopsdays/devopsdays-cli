24519104778a3faa5a50d5e96ac81adc4cde51e1
Matt Stratton <matt.stratton@gmail.com>
2017-10-05 11:11:38 -0500

    Add small functionality to helpers.GetTalks
    It only returns the filenames right now, but it’s a start
    
    Signed-off-by: Matt Stratton <matt.stratton@gmail.com>

--- helpers/get_talks.go
+++ helpers/get_talks.go
@@ -1,9 +1,27 @@
 package helpers
 
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+)
+
 // GetTalks takes in the city and year and returns a list of the talks
 func GetTalks(city, year string) []string {
 
-	s := make([]string, 3)
-	s = append(s, "This is a talk", "So is this", "Rainbows are fun", "Never mind")
+	talksDir := filepath.Join(EventContentPath(city, year), "program")
+
+	fmt.Println(talksDir)
+
+	// @TODO implement error checking in helpers.GetTalks
+	files, _ := ioutil.ReadDir(talksDir)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	s := make([]string, len(files))
+	for _, f := range files {
+		s = append(s, f.Name())
+	}
 	return s
 }
