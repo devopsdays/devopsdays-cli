c041517045e230bdb356ea2b7d9fcfe532c27cdb
Matt Stratton <matt.stratton@gmail.com>
2017-10-05 10:32:45 -0500

    Add helpers.CopyFile function
    Signed-off-by: Matt Stratton <matt.stratton@gmail.com>

---
+++ helpers/copy_file.go
@@ -0,0 +1,32 @@
+package helpers
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+// Copyfile takes a source and destination file path and copies the file. Destination needs to be complete path of file, not a directory
+func CopyFile(srcPath, destPath string) (err error) {
+	// open files r and w
+	r, err := os.Open(srcPath)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	w, err := os.Create(destPath)
+	if err != nil {
+		return err
+	}
+	defer w.Close()
+
+	// do the actual work
+	n, err := io.Copy(w, r) // <------ here !
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Copied %v bytes\n", n)
+	return nil
+}
