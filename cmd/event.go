fcf74fc00b36f61e353bf7a8c3725e997682b8b1
Matt Stratton <matt.stratton@gmail.com>
2017-10-06 16:43:31 -0500

    Remove cruft event code
    Signed-off-by: Matt Stratton <matt.stratton@gmail.com>

--- cmd/event.go
+++ cmd/event.go
@@ -1,19 +1,9 @@
 package cmd
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-	"path/filepath"
-	"strings"
-	"text/template"
-	"time"
 
-	rice "github.com/GeertJohan/go.rice"
 	"github.com/devopsdays/devopsdays-cli/event"
-	"github.com/devopsdays/devopsdays-cli/helpers"
-	"github.com/devopsdays/devopsdays-cli/helpers/paths"
 	"github.com/spf13/cobra"
 )
 
@@ -58,28 +48,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: Work your own magic here
-		// TODO: Check for args first
-		city := ""
-		year := ""
-		if city != "" {
-			if event.CheckEvent(city, year) == false {
-				log.Fatal("That city does not exist.")
-			}
-			myEvent := eventStruct(city, year)
-			editEvent(myEvent)
-		} else {
-			reader := bufio.NewReader(os.Stdin)
-			fmt.Println("Enter the city:")
-			city, _ := reader.ReadString('\n')
-			fmt.Println("Enter the year:")
-			year, _ := reader.ReadString('\n')
-			if event.CheckEvent(city, year) == false {
-				log.Fatal("That city does not exist.")
-			}
-			myEvent := eventStruct(city, year)
-			editEvent(myEvent)
-		}
+		showEvent()
 
 	},
 }
@@ -95,7 +64,6 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: Work your own magic here
 		showEvent()
 	},
 }
@@ -117,185 +85,11 @@ func init() {
 
 // Main functions go here
 
-func createEvent() {
-	fmt.Println("you would have created an event if this happened")
-}
-
 func showEvent() {
 	fmt.Println("You would have shown an event if this happened")
 }
 
-// addEvent creates a new event based upon city, year, and twitter handle.
-// It returns an empty string and an error if the event already exists.
-func addEvent(city string) (err error) {
-
-	reader := bufio.NewReader(os.Stdin) // TODO: Convert to a loop for each argument - maybe a map?
-	if city == "" {
-		fmt.Println("Enter the city: ")
-		city, _ = reader.ReadString('\n')
-	}
-	if helpers.ValidateField(city, "city") == false {
-		return fmt.Errorf("That is an invalid city. It should be less than 100 characters.")
-	}
-	t := time.Now()
-	fmt.Printf("Enter your event year (default %s): ", t.Format("2006")) // TODO: Prompt user to keep trying on invalid entry
-	eventYear, _ := reader.ReadString('\n')
-	if eventYear == "\n" {
-		eventYear = t.Format("2006")
-	}
-	if helpers.ValidateField(strings.TrimSpace(eventYear), "year") == false {
-		return fmt.Errorf("That is an invalid year. It must be four digits and between 2016 and 2030.")
-	}
-	fmt.Println("Enter your devopsdays event twitter handle (defaults to devopsdays): ")
-	eventTwitter, _ := reader.ReadString('\n')
-	if eventTwitter == "\n" {
-		eventTwitter = "devopsdays"
-	} else {
-		eventTwitter = strings.TrimSpace(strings.Replace(eventTwitter, "@", "", 1))
-	}
-	if helpers.ValidateField(eventTwitter, "twitter") == false {
-		return fmt.Errorf("That is an invalid Twitter handle. It must not contain spaces.")
-	}
-
-	// build the event data file path
-	s := []string{webdir, "/data/events/", strings.TrimSpace(eventYear), "-", strings.Replace(strings.TrimSpace(strings.ToLower(city)), " ", "-", 10), ".yml"}
-	eventDataPath := strings.Join(s, "")
-	if _, err := os.Stat(eventDataPath); err == nil {
-		return fmt.Errorf("The event already exists")
-	}
-
-	// create the event file
-	if result, err := createEventFile(city, eventYear, eventTwitter); err != nil {
-		fmt.Printf("Error: %s\n", err)
-	} else {
-		fmt.Printf("Event created for %s!!!\n", result)
-	}
-
-	// create the event content directory
-	if result, err := createEventContentDir(city, eventYear); err != nil {
-		fmt.Printf("Error: %s\n", err)
-	} else {
-		fmt.Printf("Event content directory created for %s!!!\n", result)
-	}
-
-	// create the event content files
-	contentfiles := []string{"index", "conduct", "contact", "location", "program", "propose", "registration", "sponsor"}
-	for _, contentFile := range contentfiles {
-
-		if result, err := createEventContentFile(city, eventYear, contentFile); err != nil {
-			fmt.Printf("Error: %s\n", err)
-		} else {
-			fmt.Printf("Event content file created for %s!!!\n", result)
-		}
-
-	}
-
-	return
-}
-
-func createEventFile(city, year, twitter string) (string, error) {
-
-	// find a rice.Box
-	templateBox, err := rice.FindBox("../templates")
-	if err != nil {
-		log.Fatal(err)
-	}
-	// get file contents as string
-	templateString, err := templateBox.String("event.yml.tmpl")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	s := []string{strings.TrimSpace(year), "-", strings.Replace(strings.TrimSpace(strings.ToLower(city)), " ", "-", 10)}
-	slug := strings.Join(s, "")
-	// CityClean := strings.Replace(strings.TrimSpace(strings.ToLower(city)), " ", "-", 10)
-	// t := template.Must(template.New("event.yml.tmpl").ParseFile("templates/event.yml.tmpl"))
-	// parse and execute the template
-	t, err := template.New("event.yml").Parse(templateString)
-	if err != nil {
-		log.Fatal(err)
-	}
-	data := struct {
-		City      string
-		Year      string
-		Slug      string
-		CityClean string
-		Twitter   string
-	}{
-		strings.TrimSpace(city),
-		strings.TrimSpace(year),
-		slug,
-		event.CityClean(city),
-		strings.TrimSpace(twitter),
-	}
-	f, err := os.Create(paths.EventDataPath(webdir, city, year))
-	if err != nil {
-		return "", err
-	}
-	defer f.Close()
-	t.Execute(f, data)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("Created event file for", city, "for year", year, "at", paths.EventDataPath(webdir, city, year))
-	}
-	return city, nil
-}
-
-func createEventContentDir(city, year string) (string, error) {
-	err := os.MkdirAll((paths.EventContentPath(city, year)), 0755)
-	if err != nil {
-		return "", err
-	}
-	return city, nil
-}
-
-func createEventContentFile(city, year, page string) (string, error) { // add page as an argument later
-
-	// find a rice.Box
-	templateBox, err := rice.FindBox("../templates")
-	if err != nil {
-		log.Fatal(err)
-	}
-	templateName := "events/" + page + ".md.tmpl"
-	// templateName := "index.md.tmpl"
-	// get file contents as string
-	templateString, err := templateBox.String(templateName)
-	if err != nil {
-		// log.Fatal(templateName)
-		log.Fatal(err)
-	}
-	s := []string{strings.TrimSpace(year), "-", strings.Replace(strings.TrimSpace(strings.ToLower(city)), " ", "-", 10)}
-	slug := strings.Join(s, "")
-	// t := template.Must(template.New(page+".md.tmpl").Delims("[[", "]]").ParseFiles(templateString))
-	// parse and execute the template
-	t, err := template.New(page+".md").Delims("[[", "]]").Parse(templateString)
-	if err != nil {
-		log.Fatal(err)
-	}
-	data := struct {
-		City      string
-		Year      string
-		Slug      string
-		CityClean string
-	}{
-		strings.TrimSpace(city),
-		strings.TrimSpace(year),
-		slug,
-		event.CityClean(city),
-	}
-	filePath := filepath.Join((paths.EventContentPath(city, year)), (page + ".md"))
-	f, err := os.Create(filePath)
-	if err != nil {
-		return "Cannot create", err
-	}
-	defer f.Close()
-	t.Execute(f, data)
-	if err != nil {
-		fmt.Println(err, "template execute error")
-	} else {
-		fmt.Println("Created event content file for", city, "for year", year, "at", filePath)
-	}
-	return city, nil
+func editEvent() {
+	fmt.Println("You would have edited an event if this happened")
 
 }
