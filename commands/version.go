c56da069e5fd1de4ee1fdd6013eb110e7bd1fdbe
Christopher Mills <christopher.mills@enginegroup.com>
2019-10-29 10:03:11 +0100

    Initialize go modules, docker changes
    - Setup Go Modules
    - Remove Dep requirement
    - Fix some issues with tests
    - Use raw.github.com (even though headers not rendered for time being)
    - Update goconvey.
    - Add Dockerfile changes
    
    Signed-off-by: Christopher Mills <chris.mills@enginegroup.com>

--- commands/version.go
+++ commands/version.go
@@ -7,10 +7,8 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"regexp"
 
-	"github.com/BurntSushi/toml"
 	"github.com/spf13/cobra"
 )
 
@@ -36,7 +34,6 @@ var versionCmd = &cobra.Command{
 		fmt.Println("devopsdays-cli version: ", Version)
 		fmt.Println("devopsdays-cli build: ", Build)
 		fmt.Println("hugo version: ", getHugoVersion())
-		fmt.Println("devopsdays-theme version: ", getThemeVersion())
 	},
 }
 
@@ -44,7 +41,6 @@ func showVersion() {
 	fmt.Println("devopsdays-cli version: ", Version)
 	fmt.Println("devopsdays-cli build: ", Build)
 	fmt.Println("hugo version: ", getHugoVersion())
-	fmt.Println("devopsdays-theme version: ", getThemeVersion())
 	fmt.Print("Press 'Enter' to continue...")
 	bufio.NewReader(os.Stdin).ReadBytes('\n')
 }
@@ -58,18 +54,6 @@ func getHugoVersion() (hugoVersion string) {
 	re := regexp.MustCompile("([0-9]\\.)\\w+")
 	hugoVersion = re.FindString(s)
 	return
-}
-
-func getThemeVersion() (themeVersion string) {
-	var theme Theme
-	themePath := filepath.Join(webdir, "themes", "devopsdays-theme", "theme.toml")
-	if _, err := toml.DecodeFile(themePath, &theme); err != nil {
-		fmt.Println(err)
-		return
-	}
-	themeVersion = theme.Version
-	return
-
 }
 
 func getCliVersion() (cliVersion string) {
